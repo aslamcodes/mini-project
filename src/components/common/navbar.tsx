6805a047340a1285ea86fb9420d516e1fa610b40
Mohamed Raafiq <mohamedraafiq3@gmail.com>
2022-10-19 07:35:10 +0530

    navnik

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { AiOutlineBell } from "react-icons/ai";
+import { AiOutlineBell, AiOutlinePlusCircle } from "react-icons/ai";
 const Navbar = ({ onMenuClick }) => {
   return (
-    <nav className="flex items-center gap-2 p-5 text-white bg-red-800 rounded-sm fixed top-0 left-0 right-0">
-      <div>
+    <nav className="flex justify-between p-5 text-white bg-red-800 rounded-sm fixed top-0 left-0 right-0">
+      <div className="flex items-row gap-2">
         <div onClick={onMenuClick}>
           <GiHamburgerMenu size={"1.375rem"} />
         </div>
@@ -12,9 +12,12 @@ const Navbar = ({ onMenuClick }) => {
           <p className="text-2xl ">Syner</p>
         </div>
       </div>
-      <div>
+      <div className="flex items-row gap-2">
         <div onClick={onMenuClick}>
           <AiOutlineBell size={"1.375rem"} />
+        </div>
+        <div>
+          <AiOutlinePlusCircle size={"1.375rem"} />
         </div>
       </div>
     </nav>
